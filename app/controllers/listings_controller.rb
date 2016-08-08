a7944eb8f3e0c1904825309dfc4a4ff60a6cfea8
Johannes Seikowsky <seikowsky@googlemail.com>
2016-08-08 18:50:50 +0200

    shipping basic looks

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,13 +2,14 @@ class ListingsController < ApplicationController
 
   def new_listing
     @advisor = Advisor.find(params[:id])
+    @listings = @advisor.listings
   end
 
   def new_listing_action
     @advisor = Advisor.find(params[:listing][:advisor_id])
     @listing = @advisor.listings.build(listing_params)
     if @listing.save
-      redirect_to new_listing_path(id: @advisor.id), notice: "Your listing has been created. Make another one."
+      redirect_to new_listing_path(id: @advisor.id), notice: "Listing created. Make another one, if you like."
     else
       redirect_to new_listing_path, notice: "something went wrong. try again."
     end
