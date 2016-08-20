fb77614bf9286a542ea9bab61015e46bab40d4d6
Johannes Seikowsky <seikowsky@googlemail.com>
2016-08-20 17:18:49 +0200

    attempt at mailer

--- app/mailers/general_mailer.rb
+++ app/mailers/general_mailer.rb
@@ -1,8 +1,8 @@
 class GeneralMailer < ApplicationMailer
+  default from: "founders@guidelighter.com"
 
   def successful_signup(advisor)
     @advisor = advisor
-    mail(:to => advisor.email, :subject => "Signed up!", from: "founders@guidelighter.com")
+    mail(to: advisor.email, subject: "Signed up!")
   end 
-
 end
--- config/initializers/mailer_config.rb
+++ config/initializers/mailer_config.rb
@@ -1,12 +1,12 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => "http://www.guidelighter.com/",
+  :domain               => "https://guidelighter.herokuapp.com/",
   :user_name            => "founders@guidelighter.com",
   :password             => ENV["GMAIL_PASS"],
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
 
-ActionMailer::Base.default_url_options[:host] = "https://www.guidelighter.com/" if Rails.env.production?
+ActionMailer::Base.default_url_options[:host] = "https://guidelighter.herokuapp.com/" if Rails.env.production?
 ActionMailer::Base.default_url_options[:host] = "http://localhost:3000/" if Rails.env.development?
