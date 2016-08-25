7feb740de08c20f9c279df3c73d36cfb2081f265
Johannes Seikowsky <seikowsky@googlemail.com>
2016-08-25 17:53:02 +0200

    adding and editing

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -10,9 +10,9 @@ class RequestsController < ApplicationController
     @request = @listing.requests.build(request_params)
     if @request.save
       GeneralMailer.request_notification(@request).deliver
-      redirect_to successful_request_path, notice: "Your listing request has been sent."
+      redirect_to successful_request_path, notice: "Your listing request has been sent successfully."
     else
-      redirect_to new_request_path(listing_id: @listing.id), notice: "Try again."
+      redirect_to new_request_path(listing_id: @listing.id), notice: "Please fill out both fields before send your request."
     end
   end
 
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,8 @@
 class Request < ActiveRecord::Base
 
+  # associations
   belongs_to :listing
+  # validations
+  validates_presence_of :requesting_email, :requesting_availability
 
 end
