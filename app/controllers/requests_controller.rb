d49b624f1e40d1fe9d76a62ded57284eaf7b9808
Johannes Seikowsky <seikowsky@googlemail.com>
2016-08-26 00:11:01 +0200

    tinkering

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -10,7 +10,7 @@ class RequestsController < ApplicationController
     @request = @listing.requests.build(request_params)
     if @request.save
       GeneralMailer.request_notification(@request).deliver
-      redirect_to successful_request_path, notice: "Your listing request has been sent successfully."
+      redirect_to successful_request_path, notice: "Your request has been sent successfully."
     else
       redirect_to new_request_path(listing_id: @listing.id), notice: "Please fill out both fields before send your request."
     end
