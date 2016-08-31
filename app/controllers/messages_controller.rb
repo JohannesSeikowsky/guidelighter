5edbab2dc8bef571f81dd0bab3c1293c334c7f8e
Johannes Seikowsky <seikowsky@googlemail.com>
2016-08-31 04:20:21 +0200

    mailer in resonse to contact us

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,7 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.create(contact_us: params[:message][:user_input])
+    GeneralMailer.contact_us_message(@message).deliver
     redirect_to root_path, notice: "Your message was successfully sent."
   end
 
--- app/mailers/general_mailer.rb
+++ app/mailers/general_mailer.rb
@@ -12,4 +12,10 @@ class GeneralMailer < ApplicationMailer
     @advisor = @listing.advisor
     mail(to: "founders@guidelighter.com, seikowsky@gmail.com, ogiberstein@gmail.com, surojitc@gmail.com", subject: "Request.")
   end
+
+  def contact_us_message(message)
+    @message = message
+    mail(to: "founders@guidelighter.com, seikowsky@gmail.com, ogiberstein@gmail.com, surojitc@gmail.com", subject: "Conact_Us.")
+  end
+
 end
