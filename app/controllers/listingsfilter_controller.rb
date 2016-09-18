2ed47351a3858680e26a92705aacbd244b771a3a
Johannes Seikowsky <seikowsky@googlemail.com>
2016-09-18 15:50:19 +0300

    push

--- app/controllers/listingsfilter_controller.rb
+++ app/controllers/listingsfilter_controller.rb
@@ -3,7 +3,7 @@ class ListingsfilterController < ApplicationController
   def find_listings
     @search = params[:search]
     # listings where the tags attribute includes params[:search]
-    @listings = Listing.where("tags like ?", "%#{@search}%").order("title DESC")
+    @listings = Listing.where("tags like ?", "%#{@search}%").order("updated_at DESC")
     render "search_results"
   end
 
