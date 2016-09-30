b32e4f1e712719bcc2a19f49a4160c092dc5aa0e
Johannes Seikowsky <seikowsky@googlemail.com>
2016-09-30 19:00:55 +0300

    ...

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,6 +3,6 @@ class Request < ActiveRecord::Base
   # associations
   belongs_to :listing
   # validations
-  validates_presence_of :indicated_interest, :requesting_availability, :requesting_email
+  validates_presence_of :indicated_interest, :requesting_availability, :requesting_email, :requesting_name
 
 end
