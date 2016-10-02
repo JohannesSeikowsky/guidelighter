d433c380045d48ac9c37b80beae5e3e63947ea0d
Johannes Seikowsky <seikowsky@googlemail.com>
2016-10-03 00:57:37 +0300

    push

--- app/views/search/get_results.erb.js
+++ app/views/search/get_results.erb.js
@@ -1,5 +1,5 @@
 $('#tag_search_results').css("display", "block");
 $('#mini_header').css("display", "none");
-$('#tag_search_results').html("<h6> Search: <%= @tag.capitalize %> </h6>").css("text-decoration", "underline");
+$('#tag_search_results').html("<h6> <%= @tag.upcase %> </h6>").css("text-decoration", "underline");
 $("#tag_search_results").append("<%= escape_javascript(render 'looping_through_results') %>");
 $('#tag_search_results .listing').css("background-color", "#F5EFE6").css("border", "1px solid grey");
