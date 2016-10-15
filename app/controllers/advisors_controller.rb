d30edb0efaa098bd3112be86000eefea414c26c9
Johannes Seikowsky <seikowsky@googlemail.com>
2016-10-15 22:47:05 +0300

    ..

--- app/controllers/advisors_controller.rb
+++ app/controllers/advisors_controller.rb
@@ -32,7 +32,7 @@ class AdvisorsController < ApplicationController
     if @advisor.save
       GeneralMailer.successful_signup(@advisor).deliver if Rails.env.production?
       log_in
-      redirect_to new_profile_path, notice: "Account created."
+      redirect_to new_profile_path #, notice: "Account created."
     else
       render 'signup'
     end
