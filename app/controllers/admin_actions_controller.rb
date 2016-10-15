10fdeb181ea894a623db8af5bdb4e3befc6dd02a
Johannes Seikowsky <seikowsky@googlemail.com>
2016-10-15 19:35:57 +0300

    ..

--- app/controllers/admin_actions_controller.rb
+++ app/controllers/admin_actions_controller.rb
@@ -99,10 +99,11 @@ class AdminActionsController < ApplicationController
 
   private
   def profile_params
-    params.require(:profile).permit(:description, :profile_image, :job_title)
+    params.require(:profile).permit(:description, :profile_image, :job_title, :application_help, :career_insights_help, :mentoring_help, :general_help, :session_price, :max_no_of_session)
   end
 
   def listing_params
     params.require(:listing).permit(:title, :description, :price)
   end
 end
+
