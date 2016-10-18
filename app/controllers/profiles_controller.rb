e0d1ce1f8e0dc739fa8af7cb13a2e3433b76c0ba
Johannes Seikowsky <seikowsky@googlemail.com>
2016-10-18 18:13:16 +0300

    ..

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -47,7 +47,7 @@ class ProfilesController < ApplicationController
       @advisor = Advisor.find(session[:id])
       @profile = @advisor.profile
       @profile.update(profile_params)
-      redirect_to show_profile_path(profile_id: @profile.id), notice: "Your settings have been updated successfully."
+      redirect_to show_profile_path(profile_id: @profile.id), notice: "Your updated profile:"
     else
       redirect_to login_path, notice: "Please log in."
     end
