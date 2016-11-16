3ce5cedd7f68dd29a84d185a29c906cf10993b79
Johannes Seikowsky <seikowsky@googlemail.com>
2016-11-16 15:22:21 +0200

    ..

--- app/controllers/contact_messages_controller.rb
+++ app/controllers/contact_messages_controller.rb
@@ -9,7 +9,7 @@ class ContactMessagesController < ApplicationController
   def create
     @message = ContactMessage.create(content: params[:message][:user_input], reply_address: params[:message][:email])
     GeneralMailer.contact_us_message(@message).deliver if Rails.env.production?
-    redirect_to root_path, notice: "Your message has been successfully sent."
+    redirect_to root_path, notice: "Your message has been sent. We will get back to you within a day."
   end
 
   def new_user_request
